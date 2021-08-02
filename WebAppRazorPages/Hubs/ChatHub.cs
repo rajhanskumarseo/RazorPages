ba99236880ef751981fab89e706142e2e9034cfc
Rajhans kumar <rajchaurasia@hotmail.com>
2021-08-02 22:07:40 +0530

    Add project files.

---
+++ WebAppRazorPages/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebAppRazorPages.Hubs
+{
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Groups.AddToGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public Task SendMessageToGroup(string receiver, string message)
+        {
+            return Clients.Group(receiver).SendAsync("ReceiveMessage", Context.User.Identity.Name, message);
+        }
+    }
+}
